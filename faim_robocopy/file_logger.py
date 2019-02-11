0ffbd6795ce427cdc5cb23232e2b7c06feebf315
Markus Rempfler <markus.rempfler@fmi.ch>
2019-02-11 16:27:53 +0100

    Change default logfile destination

--- faim_robocopy/file_logger.py
+++ faim_robocopy/file_logger.py
@@ -3,6 +3,24 @@ import datetime
 import logging
 
 
+def _get_logfilename():
+    '''constructs the logfile name.
+
+    '''
+    return 'Robocopy_Logfile_{}.html'.format(
+        datetime.datetime.now().strftime("%H-%M-%S"))
+
+
+def _create_logfolder_if_necessary(basedir, subdir):
+    '''create the folder basedir/subdir if it doesnt already exists.
+
+    '''
+    out_dir = os.path.join(basedir, subdir)
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    return out_dir
+
+
 def _get_logpath(user_info):
     '''constructs the logfile path.
 
@@ -11,13 +29,15 @@ def _get_logpath(user_info):
     potential_logdirs = [user_info.get('homeshare'), user_info.get('user_dir')]
     _subdir = 'Desktop'
 
-    logfilename = 'Robocopy_Logfile_{}.html'.format(
-        datetime.datetime.now().strftime("%H-%M-%S"))
+    logfilename = _get_logfilename()
 
     # check locations to log to:
     for logdir in (os.path.join(basedir, _subdir)
                    for basedir in potential_logdirs if basedir is not None):
+
         if os.path.exists(logdir):
+            logdir = _create_logfolder_if_necessary(logdir,
+                                                    'faim-robocopy-logs')
             return os.path.join(logdir, logfilename)
 
     raise IOError('Could not determine logfile path.')
