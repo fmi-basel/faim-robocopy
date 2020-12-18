08c6af78096961512d363115b2e5ae6949bd6730
Markus Rempfler <markus.rempfler@fmi.ch>
2020-12-18 18:37:34 +0100

    Refactor terminate() of RobocopyTask

--- faim_robocopy/gui/gui.py
+++ faim_robocopy/gui/gui.py
@@ -54,7 +54,6 @@ class RobocopyGUI(Frame):
     '''Main GUI.
 
     '''
-
     def __init__(self, parent, logfile, settings):
         '''initialize gui.
 
@@ -65,9 +64,9 @@ class RobocopyGUI(Frame):
 
         self.user_info = get_user_info()
         self.settings = settings
-        self.shared = SharedResources(
-            user_mail=self.user_info['user_mail'],
-            **read_params(self.user_info['user_dir']))
+        self.shared = SharedResources(user_mail=self.user_info['user_mail'],
+                                      **read_params(
+                                          self.user_info['user_dir']))
         self.shared.update_from_settings(self.settings)
 
         # Init tasks.
@@ -91,13 +90,16 @@ class RobocopyGUI(Frame):
         '''builds all components of the GUI.
 
         '''
-        Frame.__init__(
-            self, parent, width=780, height=580, borderwidth=BORDERWIDTH)
+        Frame.__init__(self,
+                       parent,
+                       width=780,
+                       height=580,
+                       borderwidth=BORDERWIDTH)
         self.pack(side=TOP, fill=BOTH, expand=True)
 
         self.horizontal_panes = PanedWindow(self, orient=HORIZONTAL)
-        self.vertical_panes = PanedWindow(
-            self.horizontal_panes, orient=VERTICAL)
+        self.vertical_panes = PanedWindow(self.horizontal_panes,
+                                          orient=VERTICAL)
         self.folder_frame = FolderSelectionUi(self.vertical_panes, self.shared)
         self.options_frame = OptionsSelectionUi(self.vertical_panes,
                                                 self.shared)
@@ -108,12 +110,18 @@ class RobocopyGUI(Frame):
             self.plugins_frame = PluginsUi(self.vertical_panes, self.plugins)
             self.vertical_panes.add(self.plugins_frame)
 
-        self.vertical_panes.pack(
-            side=LEFT, expand=True, fill=BOTH, padx=PAD, pady=PAD)
+        self.vertical_panes.pack(side=LEFT,
+                                 expand=True,
+                                 fill=BOTH,
+                                 padx=PAD,
+                                 pady=PAD)
 
         self.console_frame = ConsoleUi(self.horizontal_panes, 'Summary')
-        self.console_frame.pack(
-            side=TOP, expand=True, fill=BOTH, padx=PAD, pady=PAD)
+        self.console_frame.pack(side=TOP,
+                                expand=True,
+                                fill=BOTH,
+                                padx=PAD,
+                                pady=PAD)
 
         self.horizontal_panes.add(self.vertical_panes)
         self.horizontal_panes.add(self.console_frame)
@@ -134,26 +142,29 @@ class RobocopyGUI(Frame):
 
         '''
         button_params = dict(width=8, overrelief=RIDGE, font="arial 10")
-        self.copy_button = Button(
-            self, text='Copy', command=self.do_copy, **button_params)
-        self.copy_button.config(
-            bg="yellow green", fg="black", disabledforeground='darkgrey')
+        self.copy_button = Button(self,
+                                  text='Copy',
+                                  command=self.do_copy,
+                                  **button_params)
+        self.copy_button.config(bg="yellow green",
+                                fg="black",
+                                disabledforeground='darkgrey')
         self.copy_button.pack(side=LEFT, padx=PAD, pady=PAD)
 
-        self.cancel_button = Button(
-            self,
-            text='Abort',
-            command=self.abort,
-            state='disable',
-            **button_params)
-        self.cancel_button.config(
-            background='tomato',
-            activeforeground='black',
-            disabledforeground='darkgrey')
+        self.cancel_button = Button(self,
+                                    text='Abort',
+                                    command=self.abort,
+                                    state='disable',
+                                    **button_params)
+        self.cancel_button.config(background='tomato',
+                                  activeforeground='black',
+                                  disabledforeground='darkgrey')
         self.cancel_button.pack(side=LEFT, padx=PAD, pady=PAD)
 
-        self.settings_button = Button(
-            self, text='Settings', command=self.open_settings, **button_params)
+        self.settings_button = Button(self,
+                                      text='Settings',
+                                      command=self.open_settings,
+                                      **button_params)
         self.settings_button.pack(side='right', padx=PAD, pady=PAD)
 
     def do_copy(self):
@@ -180,22 +191,19 @@ class RobocopyGUI(Frame):
 
         # save parameters for future use.
         # TODO Shouldnt we save all settings?
-        dump_params(
-            user_dir=self.user_info['user_dir'],
-            source=self.shared.source_var.get(),
-            dest1=self.shared.dest1_var.get(),
-            dest2=self.shared.dest2_var.get())
+        dump_params(user_dir=self.user_info['user_dir'],
+                    source=self.shared.source_var.get(),
+                    dest1=self.shared.dest1_var.get(),
+                    dest2=self.shared.dest2_var.get())
 
         self.robocopy = RobocopyTask(
-            notifier=MailNotifier(
-                user_mail=self.shared.mail_var.get(),
-                logfile=self.logfile,
-                **self.settings.get_mail_kwargs()),
+            notifier=MailNotifier(user_mail=self.shared.mail_var.get(),
+                                  logfile=self.logfile,
+                                  **self.settings.get_mail_kwargs()),
             additional_flags=self.settings.get_robocopy_flags())
-        self.robocopy_thread = Thread(
-            target=decorate_callback(self.robocopy.run, self._enter_toggle,
-                                     self._exit_toggle),
-            kwargs=robocopy_kwargs)
+        self.robocopy_thread = Thread(target=decorate_callback(
+            self.robocopy.run, self._enter_toggle, self._exit_toggle),
+                                      kwargs=robocopy_kwargs)
         self.robocopy_thread.start()
 
     def open_settings(self):
@@ -213,8 +221,7 @@ class RobocopyGUI(Frame):
         '''
         logging.getLogger(__name__).info(
             'Robocopy aborted by user. Please wait ...')
-        self._stop_running_threads()
-        self._stop_robocopy_processes()
+        self._stop_robocopy_task()
         logging.getLogger(__name__).info('... abort done.')
 
     def quit(self, *args):
@@ -222,8 +229,7 @@ class RobocopyGUI(Frame):
 
         '''
         logging.getLogger(__name__).info('FAIM-robocopy terminated by user')
-        self._stop_running_threads()
-        self._stop_robocopy_processes()
+        self._stop_robocopy_task()
         self.parent.destroy()
 
     def _enter_toggle(self):
@@ -258,29 +264,8 @@ class RobocopyGUI(Frame):
             else:
                 raise
 
-    def _stop_running_threads(self):
+    def _stop_robocopy_task(self):
         '''stop all worker threads.
 
         '''
         self.robocopy.terminate()
-        # if self.robocopy_thread.is_alive():
-        #     self.robocopy_thread.join()
-
-    @staticmethod
-    def _stop_robocopy_processes():
-        '''terminate all running robocopy processes and windows consoles.
-
-        '''
-        # Legacy code
-        # TODO Check if this works / is needed.
-        # TODO Doesnt this cause issues with other instances of robocopy?
-        for proc in psutil.process_iter():
-            try:
-                if proc.name() == 'Robocopy.exe':
-                    psutil.Process(proc.pid).terminate()
-                #elif proc.name() == 'conhost.exe':
-                #    psutil.Process(proc.pid).terminate()
-            except psutil.Error as err:
-                logging.getLogger(__name__).error(
-                    'Could not terminate process %s. Error message: %s',
-                    proc.name(), str(err))
